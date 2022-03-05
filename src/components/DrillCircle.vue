e91b77dbf1973daba41bad48969632dc133b126b
fl0w3rs <47712545+fl0w3rs@users.noreply.github.com>
2022-03-05 14:24:34 +0000

    more refactor

--- src/components/DrillCircle.vue
+++ src/components/DrillCircle.vue
@@ -37,22 +37,23 @@ const drillStyle = computed(() => ({
 </script>
 
 <template>
-    <div style="overflow: hidden" class="working-round position-relative d-flex row flex-center-xy" :style="{ width: `${width}px`, height: `${height}px` }">
+    <div class="working-round position-relative d-flex row flex-center-xy of-hidden" :style="{ width: `${width}px`, height: `${height}px` }">
 
       <drill class="position-absolute" style="z-index: 1" :drillHeight="height * 0.11" :heatPercentage='heatPctg' :style="drillStyle" />
       <div class="position-absolute d-flex row flex-center-xy hw-100">
         <div v-for="index in 5" class="key-column" :style='{ width: (0.09 * width) + "px" }'>
-          <div class="align-center d-flex column key-column-bg" style="height: 50%; justify-content: flex-end">
+          <div class="align-center d-flex column key-column-top key-column-bg justify-end">
             <spring :progress="movePercentageComputed" :width="0.08 * width" />
             <latch :onTop="true" :progress="movePercentageComputed" :width="0.08 * width" />
           </div>
-          <div class="align-center column key-column-bg" style="display: flex; justify-content: flex-start">
+          <div class="align-center d-flex column key-column-bg justify-start">
             <latch :onTop="false" :progress="movePercentageComputed" :width="0.08 * width" />
           </div>
         </div>
       </div>
-      <div class="position-absolute d-flex" style="align-items: flex-end; height: 100%; width: 100%">
-        <div class="d-flex column" style="background-color: #8f8f8f; height: 20%; width: 100%">
+      <div class="position-absolute d-flex circle-footer-container">
+        <div class="circle-footer">
+            <div class="circle-subfooter"></div>
         </div>
       </div>
     </div>
@@ -71,5 +72,27 @@ const drillStyle = computed(() => ({
 
   .key-column-bg {
     background-color: #7B7B7B;
+  }
+
+  .circle-footer-container {
+    align-items: flex-end;
+    height: 100%;
+    width: 100%
+  }
+
+  .circle-footer {
+    background-color: #8f8f8f;
+    height: 20%;
+    width: 100%
+  }
+
+  .circle-subfooter {
+    background-color: rgba(87, 87, 87, 0.3);
+    height: 55%;
+    width: 100%
+  }
+
+  .key-column-top {
+    height: 50%
   }
 </style>
